c4e0cca1cbfe7237917914eb8bcfb79c441e05d5
Killian Brondy <43031530+BrKillian@users.noreply.github.com>
2018-09-27 10:27:10 +0200

    Add files via upload
    Version avec affichage des supports pas encore testé

---
+++ ApplicationDeBase-patch1/Modeles/Conteneurs/conteneurGenre.php
@@ -0,0 +1,111 @@
+﻿<?php
+include_once('Modeles/Metiers/genre.php');
+
+Class conteneurGenre
+	{
+	//ATTRIBUTS PRIVES-------------------------------------------------------------------------
+	private $lesGenres;
+
+	//CONSTRUCTEUR-----------------------------------------------------------------------------
+	public function __construct()
+		{
+		$this->lesGenres = new arrayObject();
+		}
+
+	//METHODE AJOUTANT UN genre------------------------------------------------------------------------------
+	public function ajouteUnGenre($unId‪Genre, $unLibelleGenre)
+		{
+		$unGenre = new genre($unId‪Genre, $unLibelleGenre);
+		$this->lesGenres->append($unGenre);
+
+		}
+
+	//METHODE RETOURNANT LE NOMBRE de genres-------------------------------------------------------------------------------
+	public function nbGenre()
+		{
+		return $this->lesGenres->count();
+		}
+
+//Fonction retour
+		public function listePropreDesGenres()
+	{
+	$tableau = array();
+	foreach ($this->lesGenres as $unGenre) {
+		$leLibelle = $unGenre->getLibelleGenre();
+		$laphoto = $unGenre->getidGenre();
+		$tableau[$leLibelle] = $laphoto;
+	}
+	return $tableau;
+}
+
+	//METHODE RETOURNANT LA LISTE DES Genres-----------------------------------------------------------------------------------------
+	public function listeDesGenres()
+		{
+			?>
+		<table>
+			</body>
+			<tbody>
+		<tr>
+		<?php
+
+		$tableau = $this->listePropreDesGenres();
+		$compteurTD = 0;
+		foreach ($tableau as $Libelle => $photo)
+		{
+			$compteurTD = $compteurTD+1;
+			echo '<td>< <div class="photoContainer"><img class="accueilPhotosPays" href="Affichage.php?Id='.$photo.'" src=Images/'.$photo.'>';
+			echo '<form method="post" action="index.php?vue=Videotheque&action=choixGenre?Id='.$photo.'"><div class="accueilPhotosPaysOverlay">< <input type=submit div class="textOverlay" value='.$Libelle.'>  </input></form></div></div></div></td>';
+			if (($compteurTD % 4) == 0)
+			{
+				echo '</tr><tr>';
+			}
+		}
+		?>
+			</tr>
+			</tbody>
+		</table>
+	<?php
+		}
+
+		//METHODE RETOURNANT LA LISTE DES genres DANS UNE BALISE <SELECT>------------------------------------------------------------------
+	public function lesGenresAuFormatHTML()
+		{
+		$liste = "<SELECT name = 'idGenre'>";
+		foreach ($this->lesGenres as $unGenre)
+			{
+			$liste = $liste."<OPTION value='".$unGenre->getIdGenre()."'>".$unGenre->getLibelleGenre()."</OPTION>";
+			}
+		$liste = $liste."</SELECT>";
+		return $liste;
+		}
+
+//METHODE RETOURNANT UN genre A PARTIR DE SON NUMERO--------------------------------------------
+	public function donneObjetGenreDepuisNumero($unIdGenre)
+		{
+		//initialisation d'un booléen (on part de l'hypothèse que le genre n'existe pas)
+		$trouve=false;
+		$leBonGenre=null;
+		//création d'un itérateur sur la collection lesGenres
+		$iGenre = $this->lesGenres->getIterator();
+		//TQ on a pas trouvé le genre et que l'on est pas arrivé au bout de la collection
+		while ((!$trouve)&&($iGenre->valid()))
+			{
+			//SI le numéro du genre courant correspond au numéro passé en paramètre
+			if ($iGenre->current()->getIdGenre() == $unIdGenre)
+				{
+				//maj du booléen
+				$trouve=true;
+				//sauvegarde du genre courant
+				$leBonGenre = $iGenre->current();
+
+				}
+			//SINON on passe au genre suivant
+			else
+				$iGenre->next();
+			}
+		return $leBonGenre;
+		}
+
+	}
+
+?>
---
+++ ApplicationDeBase-patch1/Modeles/Conteneurs/conteneurSupport.php
@@ -0,0 +1,129 @@
+﻿<?php
+include_once('Modeles/Metiers/support.php');
+
+Class conteneurSupport
+	{
+	//ATTRIBUTS PRIVES-------------------------------------------------------------------------
+	private $lesSupports;
+	
+	//CONSTRUCTEUR-----------------------------------------------------------------------------
+	public function __construct()
+		{
+		$this->lesSupports = new arrayObject();
+		}
+	
+	//METHODE AJOUTANT UN Support------------------------------------------------------------------------------
+	public function ajouteUnSupport($unIdSupport, $unTitreSupport, $unRealisateurSupport, $uneImageSupport, $leGenreSupport)
+		{
+		$unSupport = new support($unIdSupport, $unTitreSupport, $unRealisateurSupport, $uneImageSupport, $leGenreSupport);
+		$this->lesSupports->append($unSupport);
+			
+		}
+		
+	//METHODE RETOURNANT LE NOMBRE de Support-------------------------------------------------------------------------------
+	public function nbSupports()
+		{
+		return $this->lesSupports->count();
+		}	
+	//METHODE RETOURNANT LA LISTE DES  films-----------------------------------------------------------------------------------------
+	public function listeDesSupports()
+		{
+		$liste = '';
+		foreach ($this->lesSupports as $unSupport)
+			{	$liste = $liste.'Support N° : "'.$unSupport->getIdSupport().' -> Titre : '.$unSupport->getTitreSupport().' - '.$unSupport->getLeGenreDeSupport()->getLibelleGenre().'><br>';
+			}
+		return $liste;
+		}
+				//METHODE RETOURNANT LA LISTE DES supports DANS UNE BALISE <SELECT>------------------------------------------------------------------
+	public function lesSupportsAuFormatHTML()
+		{
+		$liste = "<SELECT name = 'idSupport'>";
+		foreach ($this->lesSupports as $unSupport)
+			{
+			$liste = $liste."<OPTION value='".$unSupport->getIdSupport()."'>".$unSupport->getTitreSupport()."</OPTION>";
+			}
+		$liste = $liste."</SELECT>";
+		return $liste;
+		}		
+	
+//METHODE RETOURNANT UN Support A PARTIR DE SON NUMERO--------------------------------------------	
+	public function donneObjetSupportDepuisNumero($unIdSupport)
+		{
+		//initialisation d'un booléen (on part de l'hypothèse que le Support n'existe pas)
+		$trouve=false;
+		$leBonSupport=null;
+		//création d'un itérateur sur la collection lesEmpruntsFilms
+		$iSupport = $this->lesSupports->getIterator();
+		//TQ on a pas trouvé le support et que l'on est pas arrivé au bout de la collection
+		while ((!$trouve)&&($iSupport->valid()))
+			{
+			//SI le numéro du support courant correspond au numéro passé en paramètre
+			if ($iSupport->current()->getIdSupport()==$unIdSupport)
+				{
+				//maj du booléen
+				$trouve=true;
+				//sauvegarde le Support courant
+				$leBonSupport = $iSupport->current();
+				
+				}
+			//SINON on passe au Support suivant
+			else
+				$iSupport->next();
+			}
+			return $leBonSupport;
+			
+		}
+	/*
+	//METHODE RETOURNANT un tableau de support a partir du numéro du genre demandé (a récuperer en get dans gestion)--------------------------------------------	
+	public function tableauSelonGenre($unIdGenre)
+		{
+				$tableau =array();
+				foreach ($this->lesSupports as $unSupport)
+				{
+					If ($unSupport->getIdSupport == $unIdGenre)
+					{
+						$leTitre = $unSupport->getTitreSupport();
+						$laPhoto = $unGenre-> getImageSupport();
+						$tableau[$leTitre] = $laPhoto;
+					}
+				}
+				return $tableau;
+			
+		}
+		*/
+	//METHODE RETOURNANT Donne la liste des supports d'un genre (depuis gestion) et renvoie l'affichage (a gestion) ----------------------------------------------------------------------------------------
+	public function listeSelonGenre($uneListe)
+		{
+			?>
+			<table>
+				</body>
+				<tbody>
+			<tr>
+			<?php
+				
+				$compteurTD = 0;
+				foreach ($uneListe as $unSupport)
+				{
+					$compteurTD = $compteurTD+1;
+					$photo = $unSupport->getImageSupport();
+					$titre = $unSupport->getTitreSupport();
+					
+					echo '<td><div class="photoContainer"><img class="accueilPhotosPays" src=Images/'.$photo.'>';
+					echo '<div class="accueilPhotosPaysOverlay"><div class="textOverlay">'.$titre.'</div></div></div></td>';
+					if (($compteurTD % 4) == 0) 
+					{
+						
+						echo '</tr><tr>';
+					}
+				}
+			?>
+				</tr>
+					</tbody>
+				</table>
+			<?php
+		}
+		
+	
+	}
+	
+?> 
